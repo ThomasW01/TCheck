5a323845dbe8ab4ae4c00544bf94654d28fac174
Thomas <82104697+ThomasW01@users.noreply.github.com>
2024-06-20 18:13:23 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {
-    WebhookUrl = "https://discord.com/api/webhooks/1251970288812294176/eHK5Wck1WekLQOsPALDP-GxDufB1yNNzYf4XQMh739W8Bl2B7Lm-yaTZQezJGpqRKkuU",
+    WebhookUrl = "",
     Core = "qb-core",
-    Webhookbackdoor = "https://discord.com/api/webhooks/1251970288812294176/eHK5Wck1WekLQOsPALDP-GxDufB1yNNzYf4XQMh739W8Bl2B7Lm-yaTZQezJGpqRKkuU",
+    Webhookbackdoor = "",
 }
